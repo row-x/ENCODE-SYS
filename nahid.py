db1d717c9413fb3713c779d4c3d4a3daf5f1d374
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-11 21:47:41 +0600

    Update nahid.py

--- nahid.py
+++ nahid.py
@@ -6,8 +6,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from bolod import expr
+    from bolod import king
     expr()
 elif bit == '32bit':
-    from bolod import expr
+    from bolod import king
     expr()
