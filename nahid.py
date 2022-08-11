3600be3149238c0b3b1c0fe421c407af57588d31
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-11 21:49:21 +0600

    Update nahid.py

--- nahid.py
+++ nahid.py
@@ -1,13 +1,3 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip2 install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from bolod import king
-    expr()
-elif bit == '32bit':
+import os
     from bolod import king
     expr()
