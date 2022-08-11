eb9bcec77cc17419c701f099181f648ef3ce0e2a
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-11 21:21:35 +0600

    Update nahid.py

--- nahid.py
+++ nahid.py
@@ -1,10 +1,5 @@
-import platform
 import os
 os.system('termux-setup-storage')
-
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("king")._login()
+__import__("king")._login()
 else:
 	exit(f' Unknow device machine {arc}')
