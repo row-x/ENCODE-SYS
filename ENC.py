e6034e34f61e232c7b6219fb836c34621e33c8fc
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-11-20 23:16:01 +0600

    Update ENC.py

--- ENC.py
+++ ENC.py
@@ -1,17 +1,4 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-    os.system('pip install bs4')
-    os.system('pip install futures')
-    os.system('gut pull')
-
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import nahid
-elif bit == '32bit':
-    import nahid
-
+import os
+os.system('git pull')
+import nahid
 nahid.login()
